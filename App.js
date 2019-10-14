f93f8b1082faf615d6b31e0bdca0a4a4b72d6e6a
Dávid Szabó <davidka1997@gmail.com>
2019-10-14 23:06:57 +0200

    Login screen POC

--- App.js
+++ App.js
@@ -1,19 +1,70 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {Text, View, Button, TextInput, ImageBackground} from 'react-native';
+import Navbar from './Navbar';
 
 export default function App() {
+  const [usernameInput, setUsernameInput] = useState('')
+  const [passwordInput, setPasswordInput] = useState('')
+
+  const doLogin = () => {
+    console.log('Username: ', usernameInput, ' Password: ', passwordInput)
+    if (usernameInput === 'Asd' && passwordInput === 'asd') {
+      return true
+    } else {
+      alert('Incorrect passowrd/username')
+      return false
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+      <ImageBackground style={{width: '100%', height: '100%'}} source={require('./assets/library_walk.jpeg')}>
+        <View style={{flex: 1}}>
+          <View style={{flex: 2, justifyContent: 'flex-start', alignItems: 'stretch'}}>
+            <Navbar />
+          </View>
+          <View style={{
+            flex: 2,
+            backgroundColor: 'white',
+            opacity: 0.8,
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}>
+            <Text style={{fontSize: 32, paddingHorizontal: 10, color: 'black'}}>DavID</Text>
+            <Text style={{fontSize: 8, paddingHorizontal: 10, color: 'black'}}>LoginEverywhere™</Text>
+            <TextInput
+                placeholder="Username"
+                placeholderTextColor="black"
+                onChangeText={(inp) => setUsernameInput(inp)}
+                style={{
+                  width: '80%',
+                  margin: 10,
+                  borderColor: 'black',
+                  borderBottomWidth: 1,
+                  textAlign: 'center'
+                }}
+            />
+            <TextInput
+                placeholder="Password"
+                placeholderTextColor="black"
+                secureTextEntry={true}
+                onChangeText={(inp) => setPasswordInput(inp)}
+                onSubmitEditing={doLogin}
+                style={{
+                  width: '80%',
+                  margin: 10,
+                  borderColor: 'black',
+                  borderBottomWidth: 1,
+                  textAlign: 'center'
+                }}
+            />
+            <Button
+                title="Login"
+                color="blue"
+                onPress={doLogin}
+            />
+          </View>
+          <View style={{flex: 2}} />
+        </View>
+      </ImageBackground>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { View, Text, Image, TouchableHighlight } from 'react-native';
+
+const Navbar = props => {
+    return (
+        <View>
+            <View style={{height: 22, backgroundColor: '#00c'}} />
+            <View
+                style={{
+                    height: 70,
+                    backgroundColor: '#00f',
+                    flexDirection: 'row',
+                    justifyContent: 'flex-start',
+                    alignContent: 'stretch'
+                }}
+            >
+                <View style={{justifyContent: 'center', marginLeft: 10}}>
+                    <TouchableHighlight onPress={(e) => alert('Hamburger menu not implemented yet')}>
+                        <Image source={require('./assets/menu.png')} />
+                    </TouchableHighlight>
+                </View>
+                <View
+                    style={{
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignContent: 'center'
+                    }}
+                >
+                    <Text style={{fontSize: 32, paddingHorizontal: 10, color: 'white'}}>DavInventory</Text>
+                    <Text style={{fontSize: 8, paddingHorizontal: 10, color: 'white'}}>TrackEverything™</Text>
+                </View>
+            </View>
+        </View>
+    )
+}
+
+export default Navbar;
